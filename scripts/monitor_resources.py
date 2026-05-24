0419ebe1f6634f49331f04ca3e7dd37701f06499
Harshal-git <harshaltambatkar11@gmail.com>
2026-05-24 13:38:11 +0530

    Added gitignore

--- scripts/monitor_resources.py
+++ scripts/monitor_resources.py
@@ -1,14 +1,25 @@
 import boto3
 import logging
+import os
 
+# Create absolute path for project directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Log file path
+LOG_FILE = os.path.join(BASE_DIR, "logs", "automation.log")
+
+# Configure logging
 logging.basicConfig(
-    filename='../logs/automation.log',
-    level=logging.INFO
+    filename=LOG_FILE,
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
 )
 
 try:
+    # Connect to AWS S3
     s3 = boto3.client('s3')
 
+    # Fetch bucket list
     response = s3.list_buckets()
 
     print("\nAvailable Buckets:\n")
@@ -20,4 +31,4 @@ try:
 
 except Exception as e:
     print("Error:", e)
-    logging.error(str(e))
+    logging.error(f"Error occurred: {str(e)}")
